20cddf4e464f64baa469bb8d5a7f522c3d7fb9ba
Mikhail_Pershin <mcpershin@yandex.ru>
2023-07-26 21:47:23 +0300

    add task solution

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+Console.WriteLine("Имеется следующий массив строк:");
+
+string[] array1 = new string[] { "365", "Number", "Year", "25", "Month", "Day" };
+string[] array2 = new string[array1.Length];
+
+// Метод сортировки массива
+
+void SortedArray(string[] array1, string[] array2)
+{
+  int count = 0;
+  for (int i = 0; i < array1.Length; i++)
+  {
+    if (array1[i].Length <= 3)
+    {
+      array2[count] = array1[i];
+      count++;
+    }
+  }
+}
+
+// Метод вывода результата
+
+void PrintArray(string[] array)
+{
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+}
+
+PrintArray(array1);
+SortedArray(array1, array2);
+Console.WriteLine();
+Console.WriteLine("Новый массив из строк: ");
+PrintArray(array2);
